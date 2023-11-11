79d2baba5e5d902edd11160096e1ef7877c18f7b
Nicolai <cwX3MfXK9YrCCc8eZk@gmail.com>
2023-11-11 19:06:26 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,123 @@
+import HeartIcon from './src/icons/HeartIcon.vue';
+import BookSkullIcon from './src/icons/BookSkullIcon.vue';
+import ScrollIcon from './src/icons/ScrollIcon.vue';
+import VrIcon from './src/icons/VrIcon.vue';
+import DungeonIcon from './src/icons/DungeonIcon.vue';
+import HatWizardIcon from './src/icons/HatWizardIcon.vue';
+import DragonIcon from './src/icons/DragonIcon.vue';
+import SkullCrossbonesIcon from './src/icons/SkullCrossbonesIcon.vue';
+import DiceIcon from './src/icons/DiceIcon.vue';
+import GamepadIcon from './src/icons/GamepadIcon.vue';
+import GhostIcon from './src/icons/GhostIcon.vue';
+import HomeIcon from './src/icons/HomeIcon.vue';
+import ShopIcon from './src/icons/ShopIcon.vue';
+import CartShoppingIcon from './src/icons/CartShoppingIcon.vue';
+import BagShoppingIcon from './src/icons/BagShoppingIcon.vue';
+import TagIcon from './src/icons/TagIcon.vue';
+import StarIcon from './src/icons/StarIcon.vue';
+import BellIcon from './src/icons/BellIcon.vue';
+import BookmarkIcon from './src/icons/BookmarkIcon.vue';
+import CameraIcon from './src/icons/CameraIcon.vue';
+import GiftIcon from './src/icons/GiftIcon.vue';
+import CommentIcon from './src/icons/CommentIcon.vue';
+import GemIcon from './src/icons/GemIcon.vue';
+import CrownIcon from './src/icons/CrownIcon.vue';
+import ShieldIcon from './src/icons/ShieldIcon.vue';
+import CoinsIcon from './src/icons/CoinsIcon.vue';
+import MoonIcon from './src/icons/MoonIcon.vue';
+import SunIcon from './src/icons/SunIcon.vue';
+import ToolboxIcon from './src/icons/ToolboxIcon.vue';
+import RotateIcon from './src/icons/RotateIcon.vue';
+import RotateLeftIcon from './src/icons/RotateLeftIcon.vue';
+import RotateRightIcon from './src/icons/RotateRightIcon.vue';
+import ArrowLeftIcon from './src/icons/ArrowLeftIcon.vue';
+import ArrowRightIcon from './src/icons/ArrowRightIcon.vue';
+import ArrowUpIcon from './src/icons/ArrowUpIcon.vue';
+import ArrowDownIcon from './src/icons/ArrowDownIcon.vue';
+import ArrowLeftRightLineIcon from './src/icons/ArrowLeftRightLineIcon.vue';
+import PaintBrushIcon from './src/icons/PaintBrushIcon.vue';
+import PersonIcon from './src/icons/PersonIcon.vue';
+import UserIcon from './src/icons/UserIcon.vue';
+import UsersIcon from './src/icons/UsersIcon.vue';
+import GearIcon from './src/icons/GearIcon.vue';
+import BarsIcon from './src/icons/BarsIcon.vue';
+import SlidersIcon from './src/icons/SlidersIcon.vue';
+import EllipsisIcon from './src/icons/EllipsisIcon.vue';
+import EllipsisVerticalIcon from './src/icons/EllipsisVerticalIcon.vue';
+import LockIcon from './src/icons/LockIcon.vue';
+import LockOpenIcon from './src/icons/LockOpenIcon.vue';
+import ClockIcon from './src/icons/ClockIcon.vue';
+import CheckmarkIcon from './src/icons/CheckmarkIcon.vue';
+import TimesIcon from './src/icons/TimesIcon.vue';
+import TrashIcon from './src/icons/TrashIcon.vue';
+import EditIcon from './src/icons/EditIcon.vue';
+import PlusIcon from './src/icons/PlusIcon.vue';
+import MinusIcon from './src/icons/MinusIcon.vue';
+import SearchIcon from './src/icons/SearchIcon.vue';
+import PlayIcon from './src/icons/PlayIcon.vue';
+import PauseIcon from './src/icons/PauseIcon.vue';
+import StopIcon from './src/icons/StopIcon.vue';
+
+export default {
+    fa: {
+        HeartIcon,
+        BookSkullIcon,
+        ScrollIcon,
+        VrIcon,
+        DungeonIcon,
+        HatWizardIcon,
+        DragonIcon,
+        SkullCrossbonesIcon,
+        DiceIcon,
+        GamepadIcon,
+        GhostIcon,
+        HomeIcon,
+        ShopIcon,
+        CartShoppingIcon,
+        BagShoppingIcon,
+        TagIcon,
+        StarIcon,
+        BellIcon,
+        BookmarkIcon,
+        CameraIcon,
+        GiftIcon,
+        CommentIcon,
+        GemIcon,
+        CrownIcon,
+        ShieldIcon,
+        CoinsIcon,
+        MoonIcon,
+        SunIcon,
+        ToolboxIcon,
+        RotateIcon,
+        RotateLeftIcon,
+        RotateRightIcon,
+        ArrowLeftIcon,
+        ArrowRightIcon,
+        ArrowUpIcon,
+        ArrowDownIcon,
+        ArrowLeftRightLineIcon,
+        PaintBrushIcon,
+        PersonIcon,
+        UserIcon,
+        UsersIcon,
+        GearIcon,
+        BarsIcon,
+        SlidersIcon,
+        EllipsisIcon,
+        EllipsisVerticalIcon,
+        LockIcon,
+        LockOpenIcon,
+        ClockIcon,
+        CheckmarkIcon,
+        TimesIcon,
+        TrashIcon,
+        EditIcon,
+        PlusIcon,
+        MinusIcon,
+        SearchIcon,
+        PlayIcon,
+        PauseIcon,
+        StopIcon
+    }
+}
--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,86 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Icons from '../index.js';
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h2>Font-Awesome Icons</h2>
+    <div class="grid">
+      <Icons.fa.HeartIcon width="2em" height="2em" />
+      <Icons.fa.BookSkullIcon width="2em" height="2em" />
+      <Icons.fa.ScrollIcon width="2em" height="2em" />
+      <Icons.fa.VrIcon width="2em" height="2em" />
+      <Icons.fa.DragonIcon width="2em" height="2em" />
+      <Icons.fa.HatWizardIcon width="2em" height="2em" />
+      <Icons.fa.SkullCrossbonesIcon width="2em" height="2em" />
+      <Icons.fa.DungeonIcon width="2em" height="2em" />
+      <Icons.fa.DiceIcon width="2em" height="2em" />
+      <Icons.fa.GamepadIcon width="2em" height="2em" />
+      <Icons.fa.GhostIcon width="2em" height="2em" />
+      <Icons.fa.HomeIcon width="2em" height="2em" />
+      <Icons.fa.ShopIcon width="2em" height="2em" />
+      <Icons.fa.BagShoppingIcon width="2em" height="2em" />
+      <Icons.fa.CartShoppingIcon width="2em" height="2em" />
+      <Icons.fa.TagIcon width="2em" height="2em" />
+      <Icons.fa.StarIcon width="2em" height="2em" />
+      <Icons.fa.BellIcon width="2em" height="2em" />
+      <Icons.fa.BookmarkIcon width="2em" height="2em" />
+      <Icons.fa.CameraIcon width="2em" height="2em" />
+      <Icons.fa.GiftIcon width="2em" height="2em" />
+      <Icons.fa.CommentIcon width="2em" height="2em" />
+      <Icons.fa.GemIcon width="2em" height="2em" />
+      <Icons.fa.CrownIcon width="2em" height="2em" />
+      <Icons.fa.ShieldIcon width="2em" height="2em" />
+      <Icons.fa.CoinsIcon width="2em" height="2em" />
+      <Icons.fa.MoonIcon width="2em" height="2em" />
+      <Icons.fa.SunIcon width="2em" height="2em" />
+      <Icons.fa.ToolboxIcon width="2em" height="2em" />
+      <Icons.fa.RotateIcon width="2em" height="2em" />
+      <Icons.fa.RotateLeftIcon width="2em" height="2em" />
+      <Icons.fa.RotateRightIcon width="2em" height="2em" />
+      <Icons.fa.ArrowLeftIcon width="2em" height="2em" />
+      <Icons.fa.ArrowRightIcon width="2em" height="2em" />
+      <Icons.fa.ArrowUpIcon width="2em" height="2em" />
+      <Icons.fa.ArrowDownIcon width="2em" height="2em" />
+      <Icons.fa.ArrowLeftRightLineIcon width="2em" height="2em" />
+      <Icons.fa.PaintBrushIcon width="2em" height="2em" />
+      <Icons.fa.PersonIcon width="2em" height="2em" />
+      <Icons.fa.UserIcon width="2em" height="2em" />
+      <Icons.fa.UsersIcon width="2em" height="2em" />
+      <Icons.fa.GearIcon width="2em" height="2em" />
+      <Icons.fa.BarsIcon width="2em" height="2em" />
+      <Icons.fa.SlidersIcon width="2em" height="2em" />
+      <Icons.fa.EllipsisIcon width="2em" height="2em" />
+      <Icons.fa.EllipsisVerticalIcon width="2em" height="2em" />
+      <Icons.fa.LockIcon width="2em" height="2em" />
+      <Icons.fa.LockOpenIcon width="2em" height="2em" />
+      <Icons.fa.ClockIcon width="2em" height="2em" />
+      <Icons.fa.CheckmarkIcon width="2em" height="2em" />
+      <Icons.fa.TimesIcon width="2em" height="2em" />
+      <Icons.fa.TrashIcon width="2em" height="2em" />
+      <Icons.fa.EditIcon width="2em" height="2em" />
+      <Icons.fa.PlusIcon width="2em" height="2em" />
+      <Icons.fa.MinusIcon width="2em" height="2em" />
+      <Icons.fa.SearchIcon width="2em" height="2em" />
+      <Icons.fa.PlayIcon width="2em" height="2em" />
+      <Icons.fa.PauseIcon width="2em" height="2em" />
+      <Icons.fa.StopIcon width="2em" height="2em" />
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+h2 {
+  text-align: center;
+  margin-top: 0;
+  margin-bottom: 2rem;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(2em, 1fr));
+  grid-gap: 1rem;
+  justify-items: center;
+  align-items: center;
 }
 </style>
